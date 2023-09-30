fc2c8e7830af8c5ea0dc72d41cfccaa50595eb31
Kapu <26711571+Kapung@users.noreply.github.com>
2023-09-30 19:15:53 +0300

    More edits

--- main.py
+++ main.py
@@ -18,27 +18,55 @@ class Window():
         self.__is_running = True
         while self.__is_running:
             self.redraw()
+        self.__root.destroy()
         
     def close(self):
         self.__is_running = False
         self.__root.protocol("WM_DELETE_WINDOW", self.close)
+    
+    def draw_line(self, line, fill_color):
+        line.draw(self.__canvas, fill_color)
 
 
 class Point():
-    def __init__(self):
-        self.x = 0
-        self.y = 0
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
 
 class Line():
     def __init__(self, x, y):
         self.x = x
         self.y = y
 
-    def draw(self, canvas, )
+    def draw(self, canvas, fill_color):
+        canvas.create_line(self.x.x, self.x.y, self.y.x, self.y.y, fill=fill_color, width=2)
+        canvas.pack()
 
+class Cell():
+    def __init__(self, win, has_left_wall=True, has_right_wall=True, has_top_wall=True, has_bottom_wall=True):
+        self.has_left_wall = has_left_wall
+        self.has_right_wall = has_right_wall
+        self.has_top_wall = has_top_wall
+        self.has_bottom_wall = has_bottom_wall
+        self._x1 = 0
+        self._x2 = 0
+        self._y1 = 0
+        self._y2 = 0
+        self._win = win
+
+    def draw(self):
+        if self.has_left_wall:
+            self.win.draw_line()
+        if self.has_right_wall:
+            self.win.draw_line()
+        if self.has_top_wall:
+            self.win.draw_line()
+        if self.has_bottom_wall:
+            self.win.draw_line()
 
 def main():
     win = Window(800, 600)
+    win.draw_line(Line(Point(15, 25), Point(200, 25)), "red")
     win.wait_for_close()
 
 main()
